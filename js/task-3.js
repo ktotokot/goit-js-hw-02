cb470a98595887dcab2cfd6c39795ff8be637498
ktotokot <shabuninae@gmail.com>
2023-12-06 19:41:55 +0200

    done

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function checkForSpam(message) {
+  const banWord1 = "spam";
+  const banWord2 = "sale";
+const result1 = message.toLowerCase().includes(banWord1);
+const result2 = message.toLowerCase().includes(banWord2);
+const result = result1 || result2;
+return result;
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
